8c5e90ee9573c908cd52b733c5ebbc6b3ea7e4b5
S19nikhilesh <nikhileshsharma2005@gmail.com>
2026-05-02 07:32:49 +0530

    problem solved:unique true issue resolved

--- src/models/users.js
+++ src/models/users.js
@@ -37,7 +37,7 @@ const userSchema=new Schema({
             type:Schema.Types.ObjectId,
             ref:'problem'
         }],
-        unique:true
+    
     },
     password:{
         type:String,
