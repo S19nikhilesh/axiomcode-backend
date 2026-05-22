dc11142da063a540f50ecd2a2d87492904587108
S19nikhilesh <nikhileshsharma2005@gmail.com>
2026-05-22 18:39:40 +0530

    contest matchhandler logic

---
+++ src/sockets/matchHandler.js
@@ -0,0 +1,89 @@
+const Problem = require('../models/problem');
+
+const queue = [];
+const activeMatches = {};
+
+const matchHandler = (io, socket) => {
+
+    socket.on('join_queue', async (userData) => {
+        const isAlreadyInQueue = queue.some(user => user.userId === userData.userId);
+        if (isAlreadyInQueue) return;
+
+        queue.push({
+            socketId: socket.id,
+            userId: userData.userId,
+            username: userData.username
+        });
+
+        if (queue.length >= 2) {
+            const player1 = queue.shift();
+            const player2 = queue.shift();
+
+            const roomId = `match_${player1.userId}_${player2.userId}`;
+
+            try {
+                const count = await Problem.countDocuments();
+                if (count === 0) {
+                    io.to(player1.socketId).to(player2.socketId).emit('error', { message: 'No problems found in DB' });
+                    return;
+                }
+
+                const randomIndex = Math.floor(Math.random() * count);
+                const randomProblem = await Problem.findOne().skip(randomIndex);
+
+                activeMatches[roomId] = {
+                    roomId,
+                    problemId: randomProblem._id,
+                    player1: { userId: player1.userId, socketId: player1.socketId, status: 'coding' },
+                    player2: { userId: player2.userId, socketId: player2.socketId, status: 'coding' },
+                    isMatch: true,
+                    winner: null
+                };
+
+                io.to(player1.socketId).emit('match_init', { roomId, problemId: randomProblem._id });
+                io.to(player2.socketId).emit('match_init', { roomId, problemId: randomProblem._id });
+
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    });
+
+
+    socket.on('match_submit', ({ roomId, userId }) => {
+        const currentMatch = activeMatches[roomId];
+        
+        if (currentMatch && !currentMatch.winner) {
+            currentMatch.winner = userId;
+
+            io.to(currentMatch.player1.socketId).emit('match_over', {
+                winnerId: userId,
+                message: currentMatch.player1.userId === userId ? 'Victory! You solved it first!' : 'Defeat! Opponent solved it first!'
+            });
+
+            io.to(currentMatch.player2.socketId).emit('match_over', {
+                winnerId: userId,
+                message: currentMatch.player2.userId === userId ? 'Victory! You solved it first!' : 'Defeat! Opponent solved it first!'
+            });
+
+            delete activeMatches[roomId];
+        }
+    });
+
+    
+    socket.on('leave_queue', (userData) => {
+        const index = queue.findIndex(user => user.userId === userData.userId);
+        if (index !== -1) {
+            queue.splice(index, 1);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        const index = queue.findIndex(user => user.socketId === socket.id);
+        if (index !== -1) {
+            queue.splice(index, 1);
+        }
+    });
+};
+
+module.exports = { matchHandler, activeMatches };
